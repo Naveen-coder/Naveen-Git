2211333adaf185ccb7651358f03db317462e6ac9
Naveen-coder <yerranaveen777@gmail.com>
2020-01-26 20:44:56 +0530

    naveen

---
+++ turtle.py
@@ -0,0 +1,16 @@
+import turtle 
+wn = turtle.Screen()        
+wn.setup(540,508)           
+
+alex = turtle.Turtle()      
+alex.shape("turtle")        
+alex.color("blue")          
+
+
+alex.backward(1000)            
+'''alex.forward(50)             
+alex.right(60)               
+alex.left(60)                
+alex.write("Hello")          
+'''
+destination="south"
